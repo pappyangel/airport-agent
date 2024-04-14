a1bb065ecf25a1631e540b3011e8ea81ff5f90c2
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-04-14 19:47:49 +0000

    added random and time factor

--- TSA-API/tsaWaitTime/Program.cs
+++ TSA-API/tsaWaitTime/Program.cs
@@ -13,22 +13,28 @@ app.UseStaticFiles();
 app.MapGet("/TSAWait/{airportCode}", (string airportCode) =>
 {
 
+  
+  Random random = new();  
   int waitTime = 0;
+
   switch (airportCode)
   {
     case "EWR":
-      waitTime = 15;
+      waitTime = random.Next(10, 16);
       break;
     case "LGA":
-      waitTime = 30;
+      waitTime = random.Next(20, 30);
       break;
     case "JFK":
-      waitTime = 45;
+      waitTime = random.Next(35, 46);
       break;
     default:
-      waitTime = 10;
+      waitTime = 2;
       break;
   }
+
+  double TimeOfDayFactor = CalcTimeOfDayFactor();
+  waitTime = (int)(waitTime * TimeOfDayFactor);
 
   return new { WaitTime = waitTime };
   //return TypedResults.Ok(new { WaitTime = waitTime });
@@ -39,3 +45,37 @@ app.MapGet("/hello", () => { return "Hello from TSA Wait Time!"; });
 
 
 app.Run();
+
+static double CalcTimeOfDayFactor()
+{
+  DateTime now = DateTime.Now;  
+  TimeSpan factorEarlyMorning = new(6, 0, 0); // 6 AM
+  TimeSpan factorLateMorning = new(10, 0, 0); // 10 AM
+  TimeSpan factorAfternoon = new(15, 0, 0); // 3 PM
+  TimeSpan factorLateNight = new(22, 0, 0); // 10 PM  
+  TimeSpan nowTime = now.TimeOfDay;
+  double TimeOfDayFactor = 1.0;
+
+  if ((nowTime > factorEarlyMorning) && (nowTime < factorLateMorning))
+  {
+    // morning rush
+    TimeOfDayFactor = 1.8;
+  }
+  else if ((nowTime > factorLateMorning) && (nowTime < factorAfternoon))
+  {
+    // mid day slow down
+    TimeOfDayFactor = 1.2;
+  }
+  else if ((nowTime > factorAfternoon) && (nowTime < factorLateNight))
+  {
+    // end of day rush
+    TimeOfDayFactor = 1.5;
+  }
+  else
+  {
+    // overnight slow down
+    TimeOfDayFactor = .8;
+  }
+
+  return TimeOfDayFactor;
+}
